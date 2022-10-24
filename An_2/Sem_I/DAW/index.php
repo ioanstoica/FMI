d22c48db05ca198d5f8508a5ac97c63bab51cb07
Stoica Ioan <stoicaioan2002@gmail.com>
2022-10-24 16:35:47 +0300

    custom ip adress

--- An_2/Sem_I/DAW/index.php
+++ An_2/Sem_I/DAW/index.php
@@ -1,41 +1,51 @@
 <?php
-    echo "Hello World! The new project is here!";
+    echo "<p> Salut, acesta este serverul lui Ioan! <p> ";
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "87654321";
+    $ipadress = getenv('REMOTE_ADDR');
+    echo "Adresa ta ip este: $ipadress\n";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, "daw");
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected successfully";
-
-    // select * from persoane
-    $sql = "SELECT * FROM persoane";
-    $result = $conn->query($sql);
-    echo $result->num_rows;
-
-    // echo every rows from persoane
-    if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["nume"]. " " . $row["prenume"]. "<br>";
-    }
-    } else {
-        echo "0 results";
+    # if ip is 192.168.43.155 echo "Salut, Stefan!";
+    if ($ipadress == "192.168.43.155") {
+        echo "Salut, Stefan!";
     }
 
-    $sql = "INSERT INTO persoane (nume, prenume) values ('Popescu', 'Ion')";
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+    echo "x";
+    
+    // $servername = "localhost"; 
+    // $username = "root"; 
+    // $password = "87654321";
 
-    $conn->close();
+    // // Create connection
+    // $conn = new mysqli($servername, $username, $password, "daw");
+
+    // // Check connection
+    // if ($conn->connect_error) {
+    //     die("Connection failed: " . $conn->connect_error);
+    // }
+    // echo "Connected successfully";
+
+    // // select * from persoane
+    // $sql = "SELECT * FROM persoane";
+    // $result = $conn->query($sql);
+    // echo $result->num_rows;
+
+    // // echo every rows from persoane
+    // if ($result->num_rows > 0) {
+    // // output data of each row
+    // while($row = $result->fetch_assoc()) {
+    //     echo "id: " . $row["id"]. " - Name: " . $row["nume"]. " " . $row["prenume"]. "<br>";
+    // }
+    // } else {
+    //     echo "0 results";
+    // }
+
+    // $sql = "INSERT INTO persoane (nume, prenume) values ('Popescu', 'Ion')";
+    // if ($conn->query($sql) === TRUE) {
+    //     echo "New record created successfully";
+    // } else {
+    // echo "Error: " . $sql . "<br>" . $conn->error;
+    // }
+
+    // $conn->close();
 
 ?>
