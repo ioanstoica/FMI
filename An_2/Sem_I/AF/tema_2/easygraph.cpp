5e20259992d5b90388f7ddc6cbbf761f4ea3220d
ioanstoica <ioanstoica89@gmail.com>
2022-11-27 01:03:34 +0200

    AF

---
+++ An_2/Sem_I/AF/tema_2/easygraph.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // open disjoint.in
+    ifstream f("disjoint.in");
+    // open disjoint.out
+    ofstream g("disjoint.out");
+    g << "gg";
+
+    // close disjoint.in
+    f.close();
+    // close disjoint.out
+    g.close();
+    return 0;
+}
--- An_2/Sem_I/AF/tema_2/retea2.py
+++ An_2/Sem_I/AF/tema_2/retea2.py
@@ -1,19 +1,52 @@
 # Stoica Ioan
 # https://www.infoarena.ro/problema/retea2
 
-# read n and m from retea.in
-fin = open("retea.in", "r")
+# read n and m from retea2.in
+fin = open("retea2.in", "r")
 n, m = map(int, fin.readline().split())
 
-# read n pairs of numbers from retea.in in a list centrale
+# read n pairs of numbers from retea2.in in a list centrale
 centrale = []
 for i in range(n):
     centrale.append(list(map(int, fin.readline().split())))
 
-# read m pairs of numbers from retea.in in a list locuinte
+# read m pairs of numbers from retea2.in in a list locuinte
 locuinte = []
 for i in range(m):
     locuinte.append(list(map(int, fin.readline().split())))
 
-# create graph with n+m nodes
-graph = [[] for i in range(n+m)]
+#create a list with m+1 element and initialize it with 2000000000.0
+dist = [2000000000.0] * (m + 1)
+
+for i in range(1,m+1):
+    for j in range(1, n+1):
+        dist[i] = min(dist[i], ((centrale[j-1][0] - locuinte[i-1][0]) ** 2 + (centrale[j-1][1] - locuinte[i-1][1]) ** 2) ** 0.5)
+
+ok = [False] * (m + 1)
+
+rez = 0
+for i in range(1, m+1):
+    minim = 2000000000.0
+    poz = 0
+    for j in range(1, m+1):
+        if ok[j]: 
+            continue
+        if dist[j] < minim:
+            minim = dist[j]
+            poz = j
+    
+    rez += minim
+    ok[poz] = True
+
+    for j in range(1, m+1):
+        if ok[j]: 
+            continue
+        dist[j] = min(dist[j], ((locuinte[poz-1][0] - locuinte[j-1][0]) ** 2 + (locuinte[poz-1][1] - locuinte[j-1][1]) ** 2) ** 0.5)
+
+import math
+# write rez to retea2.out
+fout = open("retea2.out", "w")  
+fout.write(str("{:.6f}".format(rez)))
+fout.close()
+
+# Complexity: O(n*)
