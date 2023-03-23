832c76156308ba7d33d980080900699e6d5e3a4a
ioanstoica <ioanstoica89@gmail.com>
2023-03-23 18:17:42 +0200

    refactor

--- An_2/Sem_II/AA/lab/lab3/p3.cpp
+++ An_2/Sem_II/AA/lab/lab3/p3.cpp
@@ -3,23 +3,66 @@
 
 using namespace std;
 using ll = long long;
+const double INF = 1e9;
 
-class Dreapta
+class Semiplan
 {
 public:
    ll a, b, c;
    // Overload the >> operator
-   friend istream &operator>>(istream &in, Dreapta &d)
+   friend istream &operator>>(istream &in, Semiplan &d)
    {
       in >> d.a >> d.b >> d.c;
       return in;
    }
 
    // Overload the << operator
-   friend ostream &operator<<(ostream &out, Dreapta &d)
+   friend ostream &operator<<(ostream &out, Semiplan &d)
    {
       out << d.a << " " << d.b << " " << d.c;
       return out;
+   }
+   double lim()
+   {
+      if (a == 0)
+         return (-1) * double(c) / double(b);
+      return (-1) * double(c) / double(a);
+   }
+};
+
+class IntersectieSemiplane
+{
+public:
+   double stanga = -INF, dreapta = INF, jos = -INF, sus = INF;
+   // void reset_limits()
+   // {
+   //    stanga = -INF;
+   //    dreapta = INF;
+   //    jos = -INF;
+   //    sus = INF;
+   // }
+
+   void actualizare_limite(Semiplan &d)
+   {
+      if (d.a > 0 && d.b == 0) // a*x + c < 0 =>pt a pozitiv: x < -c/a => dreapta = min(-c/a, dreapta)
+         dreapta = min(dreapta, d.lim());
+      else if (d.a < 0 && d.b == 0) // a*x + c < 0 =>pt a negativ: x > -c/a => dreapta = max(-c/a, dreapta)
+         stanga = max(stanga, d.lim());
+      else if (d.a == 0 && d.b > 0) // b*y + c < 0 =>pt b pozitiv: y < -c/b => jos = min(-c/b, jos)
+         sus = min(sus, d.lim());
+      else
+         jos = max(jos, d.lim());
+   }
+
+   string tip_intersectie()
+   {
+      if (stanga > dreapta || jos > sus)
+         return "VOID";
+
+      if (stanga == -INF || dreapta == INF || jos == -INF || sus == INF)
+         return "UNBOUNDED";
+
+      return "BOUNDED";
    }
 };
 
@@ -27,70 +70,19 @@ int main()
 {
    // ifstream cin("p3.in");
    ll n;
+   Semiplan semiplan;
+   IntersectieSemiplane intersectie;
+
    cin >> n;
-   // read n lines
-   vector<Dreapta> v(n);
+   // intersectie.reset_limits();
+
    for (ll i = 0; i < n; ++i)
-      cin >> v[i];
-
-   // sort the vector in 4 vectors
-   vector<Dreapta> os, oj, vs, vd;
-   for (auto &d : v)
    {
-      if (d.a > 0 && d.b == 0)
-         vs.push_back(d);
-      else if (d.a < 0 && d.b == 0)
-         vd.push_back(d);
-      else if (d.a == 0 && d.b > 0)
-         oj.push_back(d); // a * x + c <= 0    x <= -c / a
-      else
-         os.push_back(d);
-   }
-   // sort the vectors
-   sort(os.begin(), os.end(), [](Dreapta &d1, Dreapta &d2)
-        { return d1.c < d2.c; });
-   sort(oj.begin(), oj.end(), [](Dreapta &d1, Dreapta &d2)
-        { return d1.c > d2.c; });
-   sort(vs.begin(), vs.end(), [](Dreapta &d1, Dreapta &d2)
-        { return d1.c < d2.c; });
-   sort(vd.begin(), vd.end(), [](Dreapta &d1, Dreapta &d2)
-        { return d1.c > d2.c; });
-
-   // if vs and vd are not empty, but the first element of vs is greater than the last element of vd, then the answer is "VOID"
-
-   if (!vs.empty() && !vd.empty())
-   {
-      ll maxim = (-1) * vs[0].c;
-      ll minim = vd.back().c;
-      if (maxim < minim)
-      {
-         cout << "VOID";
-         return 0;
-      }
+      cin >> semiplan;
+      intersectie.actualizare_limite(semiplan);
    }
 
-   // if os and oj are not empty, but the first element of os is greater than the last element of oj, then the answer is "VOID"
-
-   if (!os.empty() && !oj.empty())
-   {
-      ll maxim = (-1) * oj[0].c;
-      ll minim = os.back().c;
-      if (maxim < minim)
-      {
-         cout << "VOID";
-         return 0;
-      }
-   }
-
-   // if one vector is empty, then the answer is "UNBOUNDED"
-   if (os.empty() || oj.empty() || vs.empty() || vd.empty())
-   {
-      cout << "UNBOUNDED";
-      return 0;
-   }
-
-   // if the program reaches this point, then the answer is "BOUNDED"
-   cout << "BOUNDED";
+   cout << intersectie.tip_intersectie();
 
    return 0;
 }
