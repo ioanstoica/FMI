da753729fc76d4fe5654a440f6a6e1373459a883
ioanstoica <ioanstoica89@gmail.com>
2023-04-27 17:01:27 +0300

    repair program

--- An_2/Sem_II/IA/ML/lab2.py
+++ An_2/Sem_II/IA/ML/lab2.py
@@ -20,7 +20,8 @@ naive_bayes_model = MultinomialNB()
 # X_train = [[1], [2], [1], [3], [3], [3], [3], [4]]
 # y_train = ['F', 'F', 'F', 'F', 'B', 'B', 'B', 'B']
 
-X_train = [[1], [1], [1], [1], [3], [3], [3], [3]]
+X_train = [[1,2 ], [1,2], [1,2], [1,2], [3,2], [3,2], [3,2], [3,2]]
+# X_train = [[1], [1], [1], [1], [3], [3], [3], [3]]
 y_train = ['F', 'F', 'F', 'F', 'B', 'B', 'B', 'B']
 
 X_train_array = np.array(X_train).repeat(1000000, axis=0)
@@ -30,10 +31,14 @@ print(y_train_array)
 
 naive_bayes_model.fit(X_train, y_train)
 
-X_test = [[1], [3]]
+X_test = [[1,2], [3,2]]
+# X_test = [[1], [3]]
+
 # X_test = np.array(X_test) * 1000
 
 y_pred = naive_bayes_model.predict(X_test)
 # y_pred = y_pred / 1000
 
 print(y_pred)
+
+print(naive_bayes_model.predict_proba(X_test))
