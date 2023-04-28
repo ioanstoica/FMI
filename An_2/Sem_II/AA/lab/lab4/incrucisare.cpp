49195eb5f0077f8d4418a7b0a4a9e1a1ddd3cd51
ioanstoica <ioanstoica89@gmail.com>
2023-04-28 20:43:12 +0300

    incrucisare done

--- An_2/Sem_II/AA/lab/lab4/incrucisare.cpp
+++ An_2/Sem_II/AA/lab/lab4/incrucisare.cpp
@@ -5,13 +5,34 @@ using namespace std;
 
 class Cromozom
 {
+public:
    string gena;
 };
 
-
+void incrucisare(Cromozom &x, Cromozom &y, int p)
+{
+   string x_aux = x.gena.substr(0, p) + y.gena.substr(p);
+   string y_aux = y.gena.substr(0, p) + x.gena.substr(p);
+   x.gena = x_aux;
+   y.gena = y_aux;
+}
 
 int main()
 {
-   
+   // ifstream cin("incrucisare.in");
+   // ofstream cout("incrucisare.out");
 
+   int n;
+   cin >> n;
+
+   Cromozom x, y;
+   cin >> x.gena >> y.gena;
+
+   int p;
+   cin >> p;
+
+   incrucisare(x, y, p);
+
+   cout << x.gena << '\n'
+        << y.gena << '\n';
 }
