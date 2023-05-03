7236dfe72346cd2fe58ff60cfed1be87708251f2
ioanstoica <ioanstoica89@gmail.com>
2023-05-03 14:42:05 +0300

    zoom

--- An_2/Sem_II/TW/lab8/camera.js
+++ An_2/Sem_II/TW/lab8/camera.js
@@ -7,23 +7,43 @@ function handleKeyPress(event) {
    // print the key that was pressed
    console.log(event.code);
 
-   // Verificăm codul tastelor de direcție și actualizăm poziția imaginii în consecință
+   // Obținem valorile curente ale marginilor imaginii
+   const currentMarginTop = parseInt(image.style.marginTop) || 0;
+   const currentMarginLeft = parseInt(image.style.marginLeft) || 0;
+
+   // Verificăm codul tastelor de direcție și actualizăm marginile imaginii în consecință
    switch (event.code) {
       case "ArrowLeft":
-         image.style.transform = `translateX(-${step}px)`;
+         image.style.marginLeft = `${currentMarginLeft - step}px`;
          break;
       case "ArrowRight":
-         image.style.transform = `translateX(${step}px)`;
+         image.style.marginLeft = `${currentMarginLeft + step}px`;
          break;
       case "ArrowUp":
-         image.style.transform = `translateY(-${step}px)`;
+         image.style.marginTop = `${currentMarginTop - step}px`;
          break;
       case "ArrowDown":
-         image.style.transform = `translateY(${step}px)`;
+         image.style.marginTop = `${currentMarginTop + step}px`;
          break;
-      default:
-         return;
+
    }
+
+   // let scale = 1; // zoom implicit
+   scale = parseFloat(image.style.transform.replace("scale(", "")) || 1;
+
+   if (event.code === "Equal" && event.shiftKey) {
+      // Dacă apăsăm "+" (shift + =), mărim zoom-ul cu 10%
+      scale += 0.1;
+   } else if (event.code === "Minus" && event.shiftKey) {
+      // Dacă apăsăm "shift" si "-", micșorăm zoom-ul cu 10%
+      scale -= 0.1;
+   }
+
+   // Actualizăm transformarea CSS pentru a aplica zoom-ul
+   image.style.transform = `scale(${scale})`;
+
+
+   return;
 }
 
 // Adăugăm un eveniment de ascultare pentru tastatura
