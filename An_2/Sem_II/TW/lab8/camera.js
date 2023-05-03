a3634172b0693d17a49149a20099f4deb05afddf
ioanstoica <ioanstoica89@gmail.com>
2023-05-03 14:47:21 +0300

    photo

--- An_2/Sem_II/TW/lab8/camera.js
+++ An_2/Sem_II/TW/lab8/camera.js
@@ -7,6 +7,7 @@ function handleKeyPress(event) {
    // print the key that was pressed
    console.log(event.code);
 
+   // move the image
    // Obținem valorile curente ale marginilor imaginii
    const currentMarginTop = parseInt(image.style.marginTop) || 0;
    const currentMarginLeft = parseInt(image.style.marginLeft) || 0;
@@ -28,7 +29,7 @@ function handleKeyPress(event) {
 
    }
 
-   // let scale = 1; // zoom implicit
+   // zoom 
    scale = parseFloat(image.style.transform.replace("scale(", "")) || 1;
 
    if (event.code === "Equal" && event.shiftKey) {
@@ -42,6 +43,28 @@ function handleKeyPress(event) {
    // Actualizăm transformarea CSS pentru a aplica zoom-ul
    image.style.transform = `scale(${scale})`;
 
+
+   // take a picture
+   if (event.code === "KeyS") {
+      const clone = vizor.cloneNode(true); // obiect copie a elementului div vizor
+      clone.style.position = "absolute";
+      clone.style.clip = "rect(0px," + vizor.offsetWidth + "px," + vizor.offsetHeight + "px,0px)";
+      document.body.appendChild(clone);
+
+      const audio = new Audio('camera-shutter-click.mp3');
+      audio.play();
+
+      image.style.filter = "blur(5px)";
+      vizor.style.clip = "rect(0px,0px,0px,0px)";
+      vizor.style.transition = "clip 0.5s ease-in-out";
+      setTimeout(function () {
+         vizor.style.clip = "rect(" + vizor.offsetHeight + "px," + vizor.offsetWidth + "px," + vizor.offsetHeight + "px,0px)";
+         vizor.style.transition = "clip 0.5s ease-in-out";
+         image.style.filter = "none";
+      }, 500);
+
+      return clone;
+   }
 
    return;
 }
