0eed51b05b21880183f56d83ba9e8008aa3fd056
ioanstoica <ioanstoica89@gmail.com>
2023-05-10 15:09:35 +0300

    lab 9 TW

---
+++ An_2/Sem_II/RC/DNS_Add_Blocker/dns.py
@@ -0,0 +1,15 @@
+from scapy.all import *
+
+# DNS request către google DNS
+ip = IP(dst = '8.8.8.8')
+transport = UDP(dport = 53)
+
+# rd = 1 cod de request
+dns = DNS(rd = 1)
+
+# query pentru a afla entry de tipul 
+dns_query = DNSQR(qname=b'fmi.unibuc.ro.', qtype=1, qclass=1)
+dns.qd = dns_query
+
+answer = sr1(ip / transport / dns)
+print (answer[DNS].summary())
