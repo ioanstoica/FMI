30190877eb96f690a5f2e0c13eb8f16ead89b6cf
ioanstoica <ioanstoica89@gmail.com>
2024-02-02 11:58:01 +0200

    other view

--- An_3/Sem_I/Theises/backend/backend/products/views.py
+++ An_3/Sem_I/Theises/backend/backend/products/views.py
@@ -1,17 +1,20 @@
+from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
 
 from .models import Product
 
 
 def index(request):
     latest_product_list = Product.objects.order_by("-id")[:10]
-    template = loader.get_template("products/index.html")
     context = {
         "latest_product_list": latest_product_list,
     }
 
-    return HttpResponse(template.render(context, request))
+    return render(request, "products/index.html", context)
+
 
 def detail(request, product_id):
-    return HttpResponse(f"You're looking at product with id: {product_id}.")
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, "products/detail.html", {"product": product})
