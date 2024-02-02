da50c3474b177d2a1b7e1d040c37fc3e53e39bc5
ioanstoica <ioanstoica89@gmail.com>
2024-02-02 12:53:57 +0200

    login form

--- An_3/Sem_I/Theises/backend/backend/login/views.py
+++ An_3/Sem_I/Theises/backend/backend/login/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the login index.")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("/products/")
+        else:
+            # Mesaj de eroare în caz că autentificarea eșuează
+            return HttpResponse("Autentificare eșuată")
+
+    return render(request, "login/index.html", {})
