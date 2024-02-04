9712432735bc87faa8da3dbfd7544a8f0d80e678
ioanstoica <ioanstoica89@gmail.com>
2024-02-04 19:32:24 +0200

    clean

--- An_3/Sem_I/Theises/backend/backend/backend/urls.py
+++ An_3/Sem_I/Theises/backend/backend/backend/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from signup.views import SignUpView
 
 urlpatterns = [
     path("account/", include("account.urls")),
